ef9825a969ac146edb18a354a8118fd32fa3f5a1
ji-wonLee <wjl090619@gmail.com>
2024-05-27 15:12:36 +0900

    cartDto 작성

--- sms/src/main/java/sms/dto/cartDTO.java
+++ sms/src/main/java/sms/dto/cartDTO.java
@@ -1,5 +1,47 @@
 package sms.dto;
 
-public class cartDTO {
+import java.sql.Date;
 
+public class cartDTO {
+    private String cartId;			// 카트고유번호
+    private String userId;			
+    private int totalPrice;
+    private Date cartDate;			// 카트 생성일자 및 수정일자
+    private String state;			// 카트 상태; 장바구니, 판매완료
+	
+    
+    public String getCartId() {
+		return cartId;
+	}
+	public void setCartId(String cartId) {
+		this.cartId = cartId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	public Date getCartDate() {
+		return cartDate;
+	}
+	public void setCartDate(Date cartDate) {
+		this.cartDate = cartDate;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+    
+    
+	
 }
