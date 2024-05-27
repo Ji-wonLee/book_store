274513ac760284a612d68640a73dab8e366ed8dc
DESKTOP-EI71IEB\dst03 <gad122838@gmail.com>
2024-05-27 15:40:28 +0900

    order dao, svc 올리기

---
+++ sms/src/main/java/sms/dao/impl/OrderDao.java
@@ -0,0 +1,26 @@
+package sms.dao.impl;
+
+import java.util.List;
+import sms.dto.Product;
+
+public class OrderDao {
+	public List<Product> selectInventory() {
+		return null;
+	}
+
+	public int insertOrder() {
+		return 0;
+	}
+
+	public int insertReceiveDetail(){
+		return 0;
+	}
+
+	public int receive() {
+		return 0;
+	}
+
+	public int insertOrderDetail(){
+		return 0;
+	}
+}
