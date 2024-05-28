9ab38a0c54d2a34e56cd2f905d3f7b047b774dac
DESKTOP-BF9AID6\dst08 <lokie093@gmail.com>
2024-05-28 14:20:17 +0900

    검색을 위한 분류 Entity 추가

---
+++ sms/src/main/java/sms/dto/Category.java
@@ -0,0 +1,32 @@
+package sms.dto;
+
+public class Category {
+	// 검색을 위한 카테고리 Entity
+	private String id;
+	private String name;
+	
+	public Category() {
+		
+	}
+	
+	public Category(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
