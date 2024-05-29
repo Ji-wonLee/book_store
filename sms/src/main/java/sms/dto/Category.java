4346e65fc7d5fb0a951e53e270662550d11f0c31
DESKTOP-BF9AID6\dst08 <lokie093@gmail.com>
2024-05-29 11:30:24 +0900

    Dto 수정

--- sms/src/main/java/sms/dto/Category.java
+++ sms/src/main/java/sms/dto/Category.java
@@ -2,31 +2,31 @@ package sms.dto;
 
 public class Category {
 	// 검색을 위한 카테고리 Entity
-	private String id;
-	private String name;
+	private String category_id;
+	private String category_name;
 	
 	public Category() {
 		
 	}
 	
 	public Category(String id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-	
-	public String getId() {
-		return id;
+		category_id = id;
+		category_name = name;
 	}
 
-	public void setId(String id) {
-		this.id = id;
+	public String getCategory_id() {
+		return category_id;
 	}
 
-	public String getName() {
-		return name;
+	public void setCategory_id(String category_id) {
+		this.category_id = category_id;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public String getCategory_name() {
+		return category_name;
+	}
+
+	public void setCategory_name(String category_name) {
+		this.category_name = category_name;
 	}
 }
