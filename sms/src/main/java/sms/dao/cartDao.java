5d6cebd88fa7ccf9d19c0b21ecf9e38ede85dc53
DESKTOP-EI71IEB\dst03 <gad122838@gmail.com>
2024-06-04 09:23:43 +0900

    Revert "1차 제출_예외처리, 기능 추가 수정 필요함"
    This reverts commit 15f3c1f0ce2275807c1c80730cdd8f7e069ee859.

--- sms/src/main/java/sms/dao/cartDao.java
+++ sms/src/main/java/sms/dao/cartDao.java
@@ -1,25 +1,5 @@
 package sms.dao;
 
-import java.util.List;
-
-import sms.dto.ProductDto;
-
-import sms.dto.CartDto;
-import sms.dto.PaymentDto;
-
-public interface CartDao {
-
-	List<CartDto> listCartItems(String user_id); // 장바구니 조회
-	void updateCartItemAndTotal(CartDto cartDto); // 수량 및 총가격, 시간 업데이트
-	void updateCartState(CartDto cartDto); // 장바구니 상태 업데이트
-	void createNewCart(CartDto cartDto); // 새 장바구니 생성
-	void addProductToCartDetails(CartDto cartDto); // 장바구니에 상품 추가
-	int getStock(String product_id);// 재고확인
-	//	void addProductToCart(String userId, String productId, int quantity); // 상품 장바구니에 추가
-	//	void revertToCart(String userId, String cartId); // 결제중 -> 장바구니
-	//	void updateCartStateToCompleted(String userId); // 결제 상태 완료로 업데이트
-	//	String createNewCart(String userId); // 새 장바구니 생성
-	//	String createPaymentRecord(String cartId); // 결제 기록 생성
-	//	void savePaymentInfo(PaymentDto paymentDto); // 결제 정보 저장
+public interface cartDao {
 
 }
